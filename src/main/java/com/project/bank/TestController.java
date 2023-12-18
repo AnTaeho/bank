f25ff4cdd6c9ec1751e128bc6c43d097330068d0
Taaate <an981022@naver.com>
2023-12-18 15:33:41 +0900

    chore: ci 세팅

---
+++ src/main/java/com/project/bank/TestController.java
@@ -0,0 +1,13 @@
+package com.project.bank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
